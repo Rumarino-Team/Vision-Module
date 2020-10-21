6c066cbe8d40931f9ad3dc80395a6f7ce9ae803d
Tipo <atlas@pop-os.localdomain>
2020-10-21 14:46:48 -0400

    Initial data structures

--- zed_module/include/zedmod/zedmod.hpp
+++ zed_module/include/zedmod/zedmod.hpp
@@ -2,10 +2,19 @@
 #define __ZED_MODULE__
 #include <sl/Camera.hpp>
 #include <opencv2/opencv.hpp>
-struct Video_Out {
+struct Video_Frame {
     //https://github.com/stereolabs/zed-opencv/blob/master/cpp/src/main.cpp provides a sl::Mat to cv::Mat
     //We want to use the cv::Mat to further avoid using ZED outside this module
     cv::Mat image;
     cv::Mat depth_map; //sl::Mat -> cv::Mat will contain 32bit floats
+};
+
+class ZED_Camera {
+    private:
+        Camera zed;
+    public:
+        ZED_Camera(bool record); //Inits zed, has ability to record
+        Video_Frame update(); //Gets new frame
+        char* close(); //Closes camera object and returns save path if recording
 };
 #endif
