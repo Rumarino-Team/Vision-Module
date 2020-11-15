0f57ef843b588684242c00d85f97c9e01047f3e2
Guy Garcia <guy.garcia@upr.edu>
2020-11-15 12:03:14 -0400

    Added thread functions

--- executable_module/main.cpp
+++ executable_module/main.cpp
@@ -1,7 +1,43 @@
-//Here the modules all come toguether to make the working project
-//TODO: add the pthreads flag
+#include <pthread.h>
+#include <mutex>
+#include <atomic>
+#include <condition_variable>
 
-//Following convention includes are handled this way:
 #include "zedmod/zedmod.hpp"
 #include "aimod/aimod.hpp"
 #include "apimod/apimod.hpp"
+
+void camera_stream(ZED_Camera &cam, Video_Frame &frame, std::mutex &frame_mutex, std::condition_variable &new_frame, std::atomic<bool> &running){
+    // Stop the thread properly
+    while (running) {
+        std::unique_lock<std::mutex> lock(frame_mutex);
+        frame = cam.update();
+        lock.unlock();
+        new_frame.notify_one();
+    }
+    cam.close();
+}
+
+void ai_stream(AI &ai, std::vector<DetectedObject> &objs, float confidence, Video_Frame &frame, std::mutex &obj_mutex, std::mutex &frame_mutex, std::condition_variable &new_obj, std::condition_variable &new_frame, std::atomic<bool> &running) {
+    // Stop the thread properly
+    while (running) {
+        // Handle new frame
+        std::unique_lock<std::mutex> frame_lock(frame_mutex);
+        new_frame.wait(frame_lock);
+        //Copy frame to prevent longer locking
+        Video_Frame frame_copy(frame);
+        frame_lock.unlock();
+
+        // Handle new object
+        std::unique_lock<std::mutex> obj_lock(obj_mutex);
+        objs = ai.detect(frame_copy, confidence);
+        obj_lock.unlock();
+        new_obj.notify_one();
+    }
+    ai.close();
+}
+
+int main(int argc, char* argv[]) {
+    //TODO: use arguments to initialize the ai and zed
+
+}
