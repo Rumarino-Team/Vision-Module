c833c34753db5a5d95af5baeef6dee4d57575376
Hector Miranda <hector.miranda8@upr.edu>
2021-01-28 18:39:19 -0500

    zed test now has logger

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
+#include "loggermod/loggermod.hpp"
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+    initPLOG();
     return RUN_ALL_TESTS();
 }
