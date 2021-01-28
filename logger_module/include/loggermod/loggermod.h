56a954a77307f4528f89f967edcf4c87c89d41f0
Hector Miranda <hector.miranda8@upr.edu>
2021-01-28 17:25:49 -0500

    added plog init and logging method

--- logger_module/include/loggermod/loggermod.h
+++ logger_module/include/loggermod/loggermod.h
@@ -1,10 +1,10 @@
 #ifndef __LOGGER_MOD_
 #define __LOGGER_MOD_
 #include <string>
-#include <fstream>
-#include <ctime>
-#include <experimental/filesystem>
+#include <plog/Initializers/RollingFileInitializer.h>
+#include <plog/Log.h>
 
-void RUM_log(const std::string& type, const std::string& place, const std::string& msg);
+void initPLOG();
+void pLog(std::string log);
 
 #endif
--- logger_module/src/loggermod.cpp
+++ logger_module/src/loggermod.cpp
@@ -3,28 +3,10 @@
 // Default name of file is RUM_log.txt
 const char* filename = "RUM_log.txt";
 
-/**
- * Takes three strings, one for the type of information that is imperative for the reader to
- * know such as "INFO", "DEBUG" or "TEST". Another string for where the log is being accessed,
- * for example "AI", "ZED", or "HTTP". Lastly, the third string is the log that is to be appended
- * on the log file, the message per se.
- *
- * @param type  Imperative information for the reader
- * @param place Module or place that is accessing the method
- * @param msg   Log that is to be appended to the log file
- */
+void initPLOG(){
+    plog::init(plog::info, filename);
+}
 
-void RUM_log(const std::string& type, const std::string& place, const std::string& msg){
-    // log format is as follows
-    // [TIME] \n TYPE [PLACE] MESSAGE
-    std::fstream logFile("writeable.txt", std::ios_base::app);
-    if(logFile.is_open()){
-        std::time_t log_time = time(nullptr);
-        logFile << std::string(std::asctime(std::localtime(&log_time))) + "\t" + type + "\t[" + place + "]\t" + msg << std::endl;
-        logFile.close();
-    }
-    else{
-        std::ofstream ("writeable.txt");
-        RUM_log(type, place, msg);
-    }
+void pLog(std::string log){
+    PLOGI << log;
 }
