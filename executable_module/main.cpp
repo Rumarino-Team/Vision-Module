e4fce72bd4c4ff2b2484c621fe293e4d6f651a01
Luis-Domenech <luisfabiandomenech@gmail.com>
2021-02-11 17:50:10 -0400

    Commented http api and added code for socket api

--- executable_module/main.cpp
+++ executable_module/main.cpp
@@ -4,6 +4,16 @@
 #include <chrono>
 #include <condition_variable>
 
+//Server imports
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+
 #include "zedmod/zedmod.hpp"
 #include "aimod/aimod.hpp"
 #include "httpapimod/apimod.hpp"
@@ -14,6 +24,18 @@ std::mutex frame_mutex, obj_mutex;
 // Used to handle new frames
 // NOTE: new_obj currently not used
 std::condition_variable new_frame, new_obj;
+
+//Api global variable and type
+int volatile sockfd;
+typedef struct obj_t {
+    int x;
+    int y;
+    int w; 
+    int h;
+    int id;
+    const char* name;
+    float distance;
+}__attribute__((packed)) obj;
 
 void camera_stream(ZED_Camera &cam, Video_Frame &frame, std::atomic<bool> &running){
     // Stop the thread properly
@@ -74,6 +96,183 @@ void print_help() {
     std::cout << "\t-p 8080" << std::endl;
 }
 
+obj toObj(DetectedObject o) {
+    int x = o.bounding_box.x;
+    int y = o.bounding_box.y;
+    int width = o.bounding_box.width;
+    int height = o.bounding_box.height;
+
+    obj data = {x, y, width, height, o.id, o.name, o.distance};
+
+    return data;
+}
+
+int getRoute (int sock, int debugLevel)
+{
+    //Setting variables to read socket
+    int n;
+    // char buffer[1024];
+    // bzero(buffer,1024);
+
+    int route;
+
+    //Read socket
+    n = read(sock, &route, sizeof(route));
+
+    if (n < 0) {
+        if (debugLevel >= 1) {
+            //Printf for now
+            printf("ERROR: reading from socket\n");
+        }
+    }
+
+    //Here we do api routing
+
+    //Print api call
+    if (debugLevel >= 2) {
+        //Printf for now
+        printf("INFO: API Call -> %d\n", route);
+    }
+
+    //Convert buffer to int for routing
+    return route;
+
+    // //Handy Dandy Api Routing Function
+    // execAPI(route, sock, );
+}
+
+
+void socket_api_stream(DetectedObjects &objs, std::atomic<bool> &running, int debugLevel) {
+    //Set up server first
+    
+    // Socket boilerplate
+    int newsockfd, portno, pid;
+    socklen_t clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    
+
+    //Hard code port for now
+    portno = 8080;
+
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+       if (debugLevel >= 1) {
+           //Printf for now
+           printf("ERROR: error on binding\n");
+       }
+    }
+
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+
+    pid = getpid();
+    if (pid > 0) {
+        // signal(SIGINT, SIGINT_handler);
+
+        //This is to avoid zombie processes in the server
+        signal(SIGCHLD, SIG_IGN);
+    }
+
+    if (debugLevel >= 2) {
+        printf("INFO: Server Started!\n");
+        printf("INFO: Port -> %d\n", portno);
+        fflush(stdout);
+    }
+
+    while (running) {
+
+        // Block until socket is written to
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+        
+        if (newsockfd < 0) {
+            if (debugLevel >= 1) {
+                //Printf for now
+                printf("ERROR: error on accept\n");
+            }
+        }
+
+        //Fork new child
+        pid = fork();
+        
+        if (pid < 0) {
+            if (debugLevel >= 1) {
+                //Printf for now
+                printf("ERROR: error on fork\n");
+            }
+        }
+        
+        //Let the child handle the request
+        if (pid == 0) {
+            close(sockfd);
+            int route = getRoute(newsockfd, debugLevel);
+
+
+            //We got the route number, so do magic now...
+
+            //Lock obj so it doesn't change whist sending it to client
+            std::unique_lock<std::mutex> obj_lock(obj_mutex);
+            
+            //Indicates size of struct to send
+            int header;
+            int numObjs = sizeof(objs) / sizeof(DetectedObject);
+            obj objects[numObjs];
+
+            if (numObjs > 0) {
+                for (int i = 0; i < numObjs; i++) {
+                    objects[i] = toObj(objs[i]);
+                }
+            }
+
+            // obj *objectPointer;
+            // objectPointer = objects;
+            // int header = sizeof(objectPointer);
+            header = sizeof(objects);
+
+            if (debugLevel >= 2) {
+                printf("INFO: Size of struct to send -> %ld\n", header);
+            }
+
+            //Send struct size to client for parity checking
+            if (write(newsockfd, &header, sizeof(header)) < 0) {
+                if (debugLevel >= 1) {
+                    printf("ERROR: error writing to socket\n");
+                }
+            }
+
+            //Send thee data
+            if (write(newsockfd, &objects, sizeof(objects)) < 0) {
+                if (debugLevel >= 1) {
+                    printf("ERROR: error writing to socket\n");
+                }
+            }
+
+            if (debugLevel >= 2) {
+                printf("INFO: data sent\n");
+            }
+
+            obj_lock.unlock();
+
+            exit(0);
+        }
+
+        //Parent closes connenction to new socket. Only the child should deal with the new socket
+        else close(newsockfd);
+        
+    }
+
+    //After running, we must close parent socket file descriptor
+    close(sockfd);
+}
+
+
 int main(int argc, const char* argv[]) {
     // Zed arguments
     bool live_zed = true;
@@ -86,9 +285,11 @@ int main(int argc, const char* argv[]) {
     bool m_record = false;
     int m_fps = 15;
     int confidence_percent = 60;
+    
     // API Arguments
     const char* ip = "0.0.0.0";
     int port = 8080;
+    int debugLevel = 2;
 
     for (int i = 1; i < argc; i++) {
         std::string arg = std::string(argv[i]);
@@ -155,16 +356,18 @@ int main(int argc, const char* argv[]) {
         cam_ptr.reset(new ZED_Camera(z_in));
     }
 
-    ZED_Camera& cam = *cam_ptr;
+    // ZED_Camera& cam = *cam_ptr;
+    ZED_Camera cam(false);
     Video_Frame frame;
 
     // Initialize AI
-    AI ai(model, m_record, m_out, m_fps);
+    // AI ai(model, m_record, m_out, m_fps);
+    AI ai("../test/media/RUBBER-DUCKY", true, "ai_output.avi", 15);
     DetectedObjects objs;
     float conf = float(confidence_percent) / 100;
 
     // Initialize API
-    API api(obj_mutex, objs);
+    // API api(obj_mutex, objs);
 
     // Use argument variables
     static std::atomic<bool> running = true;
@@ -172,9 +375,12 @@ int main(int argc, const char* argv[]) {
     // Since threads copy arguments we must pass them by reference.
     std::thread camera_thread(camera_stream, std::ref(cam), std::ref(frame), std::ref(running));
     std::thread ai_thread(ai_stream, std::ref(ai), conf, std::ref(objs), std::ref(frame), std::ref(running));
+    std::thread socket_api_thread(socket_api_stream, std::ref(objs), std::ref(running), std::ref(debugLevel));
 
-    api.start(ip, port);
+    //api.start(ip, port);
+    
     running = false;
     camera_thread.join();
     ai_thread.join();
+    socket_api_thread.join();
 }
