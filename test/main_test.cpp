f6da549a2070ed8c87059e71039020472d2aead5
Hector Miranda <hector.miranda8@upr.edu>
2021-03-11 12:40:00 -0500

    fixed plog init on main tester

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -3,6 +3,16 @@
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    initPLOG();
+
+    //Logger
+    std::string maxSeverity = "info";
+    for (int i = 1; i < argc; i++) {
+        std::string arg = std::string(argv[i]);
+        if (arg == "-d" || arg == "--debug"){
+            maxSeverity = "debug";
+        }
+    }
+    initPLOG(maxSeverity);
+
     return RUN_ALL_TESTS();
 }
